6367df19aae046c97f2fc986c58af14aa144344e
jloh02 <36648707+jloh02@users.noreply.github.com>
2021-11-12 16:40:24 +0800

    Changed return values to riot messages

--- web-backend/partyjoin.go
+++ web-backend/partyjoin.go
@@ -14,7 +14,7 @@ func partyjoin(client *http.Client, w http.ResponseWriter, r *http.Request, cred
 	reqStatus, message := join(fmt.Sprintf("/parties/%s/request", req.PartyId), client, r, creds, req)
 
 	//If open party, joinparty directly
-	if reqStatus == 400 && strings.Contains(message, "REQUEST_OPEN_PARTY") {
+	if reqStatus == 400 && strings.Contains(string(message), "REQUEST_OPEN_PARTY") {
 		reqStatus, message = join(fmt.Sprintf("/players/%s/joinparty/%s", creds.PUUID, req.PartyId), client, r, creds, req)
 	}
 
@@ -22,13 +22,13 @@ func partyjoin(client *http.Client, w http.ResponseWriter, r *http.Request, cred
 		log.Print(message)
 	}
 
-	w.WriteHeader(reqStatus)
+	w.Write(message)
 }
 
-func join(endpoint string, client *http.Client, r *http.Request, creds valorantCredentials, req valorantRequest) (int, string) {
+func join(endpoint string, client *http.Client, r *http.Request, creds valorantCredentials, req valorantRequest) (int, []byte) {
 	ptJoinReq, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://glz-%s-1.%s.a.pvp.net/parties/v1%s", req.Region, req.Region, endpoint), bytes.NewBufferString(fmt.Sprintf("{\"Subjects\":[\"%s\"]}", req.OwnerPuuid)))
 	if err != nil {
-		return 500, "Unknown internal error"
+		return 500, []byte("Unknown internal error")
 	}
 	ptJoinReq.Header.Add("Content-Type", "application/json")
 	ptJoinReq.Header.Add("Authorization", creds.Token)
@@ -36,8 +36,8 @@ func join(endpoint string, client *http.Client, r *http.Request, creds valorantC
 
 	if resp, err := client.Do(ptJoinReq); err != nil || resp.StatusCode != 200 {
 		r, _ := ioutil.ReadAll(resp.Body)
-		return resp.StatusCode, string(r)
+		return resp.StatusCode, r
 	} else {
-		return 200, "Success"
+		return 200, []byte("Success")
 	}
 }
