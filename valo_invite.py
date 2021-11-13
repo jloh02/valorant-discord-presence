e13723d78b371ec951f6a5bd8d432d0e14a496f1
jloh02 <36648707+jloh02@users.noreply.github.com>
2021-11-13 16:22:33 +0800

    Added python utility to retrieve local party details

---
+++ valo_invite.py
@@ -0,0 +1,25 @@
+import os
+import base64
+import requests
+import json
+
+lockfilePath = os.path.join(os.getenv('LOCALAPPDATA'), R'Riot Games\Riot Client\Config\lockfile')
+
+lockfileF = open(lockfilePath)
+data = lockfileF.read().split(':')
+keys = ['name', 'PID', 'port', 'password', 'protocol']
+lockfile = dict(zip(keys, data))
+
+headers = {}
+headers['Authorization'] = 'Basic ' + base64.b64encode(('riot:' + lockfile['password']).encode()).decode()
+response = requests.get("https://127.0.0.1:{port}/chat/v1/session".format(port=lockfile['port']), headers=headers, verify=False)
+puuid = response.json()['puuid']
+
+response = requests.get("https://127.0.0.1:{port}/chat/v4/presences".format(port=lockfile['port']), headers=headers, verify=False)
+presences = response.json()
+
+for presence in presences['presences']:
+    if presence['puuid'] == puuid:    # compares to the PUUID we got in the previous response to aviod setting someone else's state as our status
+        party_id = json.loads(base64.b64decode(presence['private']))['partyId']
+        
+print("https://jloh02.github.io/valorant-discord-presence?puuid={puuid}&party={party}&region={region}".format(party=party_id, puuid=puuid, region='ap'))
